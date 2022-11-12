b31a708d321634badc2e7b836c35e4c4eea6c5cd
vishal singh <102172720+vishALL0@users.noreply.github.com>
2022-11-12 13:28:51 +0530

    Update odi.py

--- odi.py
+++ odi.py
@@ -1,16 +1,23 @@
+#import python laibraries
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 from csv import writer
+#url link of cricbuzz 
+
 url="https://www.cricbuzz.com/cricket-stats/icc-rankings/women/batting"
 page=requests.get(url)
 soup=BeautifulSoup(page.content,'html.parser')
 name=soup.find_all('div',class_="cb-col cb-col-100 cb-font-14 cb-lst-itm text-center")
 
+#now creating a csv file to store data
+
 with open("odi_women.csv","w") as f:
     thewriter=writer(f)
     header=["Position","Player Name","Country","Rating"]
     thewriter.writerow(header)
+    
+    
     for i in name:
         player=i.find('a',class_="text-hvr-underline text-bold cb-font-16").text
         country=i.find('div',class_="cb-font-12 text-gray").text
